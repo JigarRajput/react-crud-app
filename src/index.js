9780d23292cfc43d73ab6f4f172ce61498095904
JigarRajput <rajputjigar455@gmail.com>
2023-02-04 22:36:03 +0530

    trying

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './App.css';
 import App from './App';
-
+// jai Ganesh
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
